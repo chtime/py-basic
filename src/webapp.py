7f7f66352802679b7543b4663b4cc653dfae1a80
Till Meyer zu Westram <till.meyerzuwestram@zuehlke.com>
2022-03-29 11:09:37 +0200

    print hostname

--- src/webapp.py
+++ src/webapp.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
 
+import logging
+import socket
+
 from flask import Flask
 
+logger = logging.getLogger(__name__)
 flask_app = Flask(__name__)
+
 
 @flask_app.route("/")
 def hello_world():
-    return {"content": "hello, world!"}
+    """Hello World!"""
+    try:
+        hostname = socket.gethostname()
+    except Exception as e:
+        logger.warning(e)
+        hostname = "n/a"
 
+    return {"content": "hello, world!", "host": hostname}
